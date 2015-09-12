383400d007e666bd90594d58bc0889168f79105e
jwareing <jwareing89@gmail.com>
2015-09-12 12:22:45 -0700

    Added client interface

--- server/db/index.js
+++ server/db/index.js
@@ -11,9 +11,9 @@ var dbConnection = mysql.createConnection({
   });
 exports.getMessages = function (res){
   console.log('getMessages is running');
-  dbConnection.query("SELECT * FROM messages", [], function(err, results) {
+  dbConnection.query("SELECT text, username FROM messages m INNER JOIN users u ON m.U_ID_users = u.U_ID;", [], function(err, results) {
     if (err){throw err;}
-    res.send(200, results);
+    res.status(200).send(results);
   });
 };
 
@@ -21,13 +21,13 @@ exports.getUsers = function (res){
   console.log('getUsers is running');
   dbConnection.query("SELECT * FROM users", [], function(err, results) {
     if (err){throw err;}
-    res.send(200, results);
+    res.status(200).send(results);
   });
 };
 
 exports.insertMessages = function (data, res){
   console.log('insertMessages is running');
-  dbConnection.query("INSERT INTO messages (text, U_ID_users) values (" + JSON.stringify(data.message) + 
+  dbConnection.query("INSERT INTO messages (text, U_ID_users) values (" + JSON.stringify(data.text) + 
     ", (select U_ID from users u where u.username = " + JSON.stringify(data.username) + "));", [],
     function(err, results) {
       if (err){throw err;}
